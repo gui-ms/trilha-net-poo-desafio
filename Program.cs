0ba592de67d78a2c60bc8ec3e22fc99cb8c8bf71
Guilherme Martins <gui.martins.soares@gmail.com>
2024-09-05 20:41:42 -0300

    Added tests to check both phone classes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Testes Nokia");
+Nokia n1 = new Nokia("123456789", "Tijolo", "abcd", 64);
+n1.InstalarAplicativo("Netflix");
+n1.Ligar();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Testes Iphone");
+Iphone i1 = new Iphone("123456789", "Iphone 8000", "abcd", 32);
+i1.InstalarAplicativo("IPhood");
+i1.Ligar();
